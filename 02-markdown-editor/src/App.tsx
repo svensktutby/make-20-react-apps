ff1944757a7aff8ddbc0f126a5d7597be5cbcd69
svensktutby <svensk.tut.by@gmail.com>
2021-01-17 21:29:14 +0300

    init: start task 02 - Markdown Editor

---
+++ 02-markdown-editor/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './App.css';
+
+export const App: FC = () => {
+  return (
+    <div className="app">
+      <textarea />
+
+      <div className="preview" />
+    </div>
+  );
+};
