4773dc90ffb5e5ebccd2fafab722c410818e6051
svensktutby <svensk.tut.by@gmail.com>
2021-01-18 13:28:38 +0300

    feat: finish task 02 - Markdown Editor

--- 02-markdown-editor/src/App.tsx
+++ 02-markdown-editor/src/App.tsx
@@ -1,12 +1,26 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
+import marked from 'marked';
+import ReactMarkdown from 'react-markdown';
+
 import './App.css';
 
 export const App: FC = () => {
+  const [markdown, setMarkdown] = useState('# sup');
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdown(e.currentTarget.value);
+  };
+
   return (
     <div className="app">
-      <textarea />
+      <textarea value={markdown} onChange={handleChange} />
 
-      <div className="preview" />
+      {/* <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+      /> */}
+
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 };
