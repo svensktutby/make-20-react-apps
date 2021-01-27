872312252c3c0e918bac51ccc10964605b3af8dc
svensktutby <svensk.tut.by@gmail.com>
2021-01-27 13:56:08 +0300

    init: start task 06 - Infinite Image Gallery

---
+++ 06-infinite-image-gallery/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+
+import './App.css';
+
+export const App: FC = () => {
+  return (
+    <div className="app">
+      <h1>Unsplash Image Gallery!</h1>
+
+      <form>
+        <input type="text" placeholder="Search Unsplash..." />
+        <button type="button">Search</button>
+      </form>
+
+      <div className="image-grid">
+        {[...Array(100)].map((_, index) => (
+          <div className="image" key={index}>
+            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
